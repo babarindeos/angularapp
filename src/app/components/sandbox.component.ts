07221ead2a1a4b7af97ab8a3a1c338b1360feeb9
Seyibabs <seyibabs.ng@gmail.com>
2022-07-06 07:27:18 +0100

    Interpolation

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -2,9 +2,34 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'sandbox',
-    template: `<h1>Hello World</h1>`
+    template: `<h1>{{ name }} is {{ age }} years old.</h1>
+               <h2> {{ person.firstname }} {{ person.lastname }} </h2>
+               <ul>
+                    <li>Helo World</li>
+                    <li> {{ 1 + 1}}</li>
+                    <li> {{ showAge() }}</li>
+               </ul>
+    `
 })
 
 export class SandboxComponent{
+    name = "John Doe";
+    age = 35;
 
+    person = {firstname: 'Steve', lastname: 'Smith'};
+
+    constructor(){
+        console.log('Constructor ran ...');
+        this.age = 47;
+        this.hasBirthday();
+        this.hasBirthday();
+    }
+
+    hasBirthday(){
+        this.age++;
+    }
+
+    showAge(){
+        return this.age;
+    }
 }
