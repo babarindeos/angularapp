a2dfb96c9d06c9038a9433c65e616b76606fcfa6
Seyibabs <seyibabs.ng@gmail.com>
2022-07-06 09:08:52 +0100

    Objects Interfaces

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -1,45 +1,53 @@
 import { Component } from '@angular/core';
+import { Customer } from './Customer';
 
 @Component({
     selector: 'sandbox',
     template: `<h1>{{ name }} is {{ age }} years old.</h1>
-               <h2> {{ person.firstname }} {{ person.lastname }} </h2>
-               <ul>
-                    <li>Helo World</li>
-                    <li> {{ 1 + 1}}</li>
-                    <li> {{ showAge() }}</li>
-                    <li> {{ myitems }} </li>
-               </ul>
+    <ul>
+        <li> {{ customer.name }} -  {{ customer.email }}</li>
+    </ul>
+               
     `
 })
 
 export class SandboxComponent{
-    name: string = "John Doe";
-    age: number = 35;
-    isvalid: boolean = false;
-    people: object = [];
-    things: any = {};
-    mynumbers: Array<number> = [1,2,3,4];
-    skills: Array<string> = ['string','test','jump'];
-    myitems: Array<any> = ['seyi', 2, true];
-    //purse: Tuple
-    you: undefined;
-    //testing: null
 
-    person = {firstname: 'Steve', lastname: 'Smith'};
+    name: string = "Joe Doe";
+    age: number = 35;
+
+    //customer: { id: number, name: string, email: string };
+    customer: Customer;
+
+    customers: Customer[];
 
     constructor(){
-        console.log('Constructor ran ...');
-        this.age = 47;
-        this.hasBirthday();
-        this.hasBirthday();
-    }
+        this.customer = {
+            id: 1,
+            name: 'Seyi',
+            email: 'seyibabs@gmail.com',
+        };
 
-    hasBirthday(){
-        this.age++;
-    }
+        this.customers = [
+            {
+                id: 1,
+                name: 'Seyi',
+                email: 'seyibabs@gmail.com',
+            },
+            {
+                id: 2,
+                name: 'Brad',
+                email: 'brad@gmail.com',
+            },
+            {
+                id: 3,
+                name: 'Kings',
+                email: 'Kings@gmail.com',
+            }
+        ];
+    }   
 
-    showAge(){
-        return this.age;
-    }
+   
 }
+
+
